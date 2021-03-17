4defc554cdc16e75e72c1b91c824c8329465be3c
“nahuel” <n.gorosito@yahoo.com>
2021-03-17 13:15:04 -0400

    Initialize RootQueryType + Schema

---
+++ server/schema/root_query_type.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const graphql = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLID,
+  GraphQLList,
+  GraphQLNonNull,
+} = graphql;
+
+const GodType = require("./god_type");
+const God = mongoose.model("god");
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: () => ({
+    gods: {
+      type: new GraphQLList(GodType),
+      resolve() {
+        return God.find({});
+      },
+    },
+    god: {
+      type: GodType,
+      args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+      resolve(parentValue, { id }) {
+        return God.findById(id);
+      },
+    },
+  }),
+});
+
+module.exports = RootQuery;
