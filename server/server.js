c282cbaa3cffa502ac7c1081991b7027221dd24b
“nahuel” <n.gorosito@yahoo.com>
2021-03-17 13:28:49 -0400

    Connect server with schema

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const db = require("../config/keys").MONGO_URI;
+const expressGraphQL = require("express-graphql").graphqlHTTP;
+const models = require("./models");
+const schema = require("./schema/schema");
 
 const app = express();
 
@@ -15,5 +18,13 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use(bodyParser.json());
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema,
+    graphiql: true,
+  })
+);
 
 module.exports = app;
