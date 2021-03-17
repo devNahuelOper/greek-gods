767bf7cbfac82c5aa02ec2be096c765a640c1028
“nahuel” <n.gorosito@yahoo.com>
2021-03-17 13:02:54 -0400

    Add seed_data to gitignore

--- index.js
+++ index.js
@@ -5,5 +5,3 @@ const app = require("./server/server");
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
-
-// mongodb+srv://dev:oJF8JxZC9wpHu6tn;@cluster0.ncp6z.mongodb.net/greek-gods?retryWrites=true&w=majority
