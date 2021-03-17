3b5e1ffbddc754e209a7b90d2b2dc5cde21a8c8f
“nahuel” <n.gorosito@yahoo.com>
2021-03-17 11:49:47 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
+
+// oJF8JxZC9wpHu6tn;
+
+// mongodb+srv://dev:oJF8JxZC9wpHu6tn;@cluster0.ncp6z.mongodb.net/greek-gods?retryWrites=true&w=majority
