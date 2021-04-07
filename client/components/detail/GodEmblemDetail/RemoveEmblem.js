c055500c292f6a7fa71fd19040dfde990176f114
“nahuel” <n.gorosito@yahoo.com>
2021-04-07 09:47:35 -0400

    Clean up removeGodEmblem component

--- client/components/detail/GodEmblemDetail/RemoveEmblem.js
+++ client/components/detail/GodEmblemDetail/RemoveEmblem.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Mutation } from "react-apollo";
-import Emblems from "./Emblems";
 import EditTools from "../EditTools";
 
 import Mutations from "../../../graphql/mutations";
@@ -12,6 +11,21 @@ class RemoveEmblem extends React.Component {
     this.state = {
       emblems: this.props.emblems || [],
     };
+  }
+
+  removeEmblem(e, emblemId, removeGodEmblem) {
+    e.preventDefault();
+    const { emblems } = this.state;
+    const { id } = this.props;
+
+    removeGodEmblem({
+      variables: {
+        godId: id,
+        emblemId,
+      },
+    }).then(() =>
+      this.setState({ emblems: emblems.filter((emb) => emb.id != emblemId) })
+    );
   }
 
   render() {
@@ -28,19 +42,9 @@ class RemoveEmblem extends React.Component {
                 <li key={emblem.id}>
                   <h4>{emblem.name}</h4>
                   <EditTools
-                    onClick={(e) => {
-                      e.preventDefault();
-                      removeGodEmblem({
-                        variables: {
-                          godId: id,
-                          emblemId: emblem.id,
-                        },
-                      }).then(() =>
-                        this.setState({
-                          emblems: emblems.filter((emb) => emb.id != emblem.id),
-                        })
-                      );
-                    }}
+                    onClick={(e) =>
+                      this.removeEmblem(e, emblem.id, removeGodEmblem)
+                    }
                     title={`Remove ${emblem.name}`}
                     icon="x"
                   />
