b744c26893b867cc6f539802062ca339f98d9413
“nahuel” <n.gorosito@yahoo.com>
2021-04-07 09:35:15 -0400

    Build RemoveGodEmblem component

--- client/components/detail/GodEmblemDetail/RemoveEmblem.js
+++ client/components/detail/GodEmblemDetail/RemoveEmblem.js
@@ -7,11 +7,11 @@ import Mutations from "../../../graphql/mutations";
 const { REMOVE_GOD_EMBLEM } = Mutations;
 
 class RemoveEmblem extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      emblems: this.props.emblems || []
-    }
+      emblems: this.props.emblems || [],
+    };
   }
 
   render() {
@@ -22,23 +22,35 @@ class RemoveEmblem extends React.Component {
       <React.Fragment>
         <Mutation mutation={REMOVE_GOD_EMBLEM}>
           {(removeGodEmblem, data) => (
-            <Emblems emblems={emblems} listChildren >
-              <EditTools onClick={e => {
-                e.preventDefault();
-                // removeGodEmblem({
-                //   variables: {
-                //     godId: id,
-                //   }
-                // })
-              }}
-              title="Remove Emblem"
-              icon="x"
-              />
-            </Emblems>
+            <ul className="god-emblems">
+              <h3>Emblems: </h3>
+              {emblems.map((emblem) => (
+                <li key={emblem.id}>
+                  <h4>{emblem.name}</h4>
+                  <EditTools
+                    onClick={(e) => {
+                      e.preventDefault();
+                      removeGodEmblem({
+                        variables: {
+                          godId: id,
+                          emblemId: emblem.id,
+                        },
+                      }).then(() =>
+                        this.setState({
+                          emblems: emblems.filter((emb) => emb.id != emblem.id),
+                        })
+                      );
+                    }}
+                    title={`Remove ${emblem.name}`}
+                    icon="x"
+                  />
+                </li>
+              ))}
+            </ul>
           )}
         </Mutation>
       </React.Fragment>
-    )
+    );
   }
 }
 
